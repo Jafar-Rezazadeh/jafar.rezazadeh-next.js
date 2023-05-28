9ca93b1c9b72a48124745a083c9f73a27e09f079
Jafar-Rezazadeh <jafarrezazadeh76@gmail.com>
2023-05-28 13:18:35 +0430

    initial

--- comps/Layout.js
+++ comps/Layout.js
@@ -1,14 +1,20 @@
 import { motion } from "framer-motion";
 import NavBar from "./NavBar";
 import Footer from "./Footer";
+import Head from "next/head";
+import { useRouter } from "next/router";
 
 const Layout = ({ children }) => {
+  const router = useRouter();
   return (
     <motion.div
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       exit={{ opacity: 0 }}
     >
+      <Head>
+        <title>jafar.rezazadeh {router.asPath}</title>
+      </Head>
       <NavBar />
       {children}
       <Footer />
