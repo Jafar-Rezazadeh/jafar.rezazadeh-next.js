b72c47d888a4f75e83f7814aaf9d16e8cfb4be8e
Jafar-Rezazadeh <jafarrezazadeh76@gmail.com>
2023-05-30 14:40:44 +0430

    updated

--- comps/Layout.js
+++ comps/Layout.js
@@ -3,17 +3,21 @@ import NavBar from "./NavBar";
 import Footer from "./Footer";
 import Head from "next/head";
 import { useRouter } from "next/router";
+import { Suspense } from "react";
+import Loading from "@/pages/loading";
 
 const Layout = ({ children }) => {
   const router = useRouter();
   return (
     <>
       <Head>
-        <title>jafar.rezazadeh {router.asPath}</title>
+        <title>jafar.rezazadeh</title>
       </Head>
-      <NavBar />
-      {children}
-      <Footer />
+      <Suspense fallback={<Loading />}>
+        <NavBar />
+        {children}
+        <Footer />
+      </Suspense>
     </>
   );
 };
